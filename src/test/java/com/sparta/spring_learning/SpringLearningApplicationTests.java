7253bcdb5a517d781cfc0a7add4a9f3200a4b4be
kudongku <kudongku@naver.com>
2024-01-16 20:43:06 +0900

    init commit

---
+++ src/test/java/com/sparta/spring_learning/SpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
